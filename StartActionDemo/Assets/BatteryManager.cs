6528b95c91eeaaa08e66619fc71ff0b25e6278d9
俊伟 娄 <958868128@qq.com>
2019-01-07 15:34:12 +0800

    qwe
    qwe

---
+++ StartActionDemo/Assets/BatteryManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BatteryManager : MonoBehaviour {
+
+    public Text Power;
+    public Text Time;
+    string _time = string.Empty;
+    string _battery = string.Empty;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("UpdataBattery");
+        StartCoroutine("UpdataTime");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    IEnumerator UpdataBattery()
+    {
+        while (true)
+        {
+            _battery = GetBatteryLevel().ToString();
+            Power.text = "Battery "+_battery + "%";
+            yield return new WaitForSeconds(120f);
+        }
+    }
+
+    int GetBatteryLevel()
+    {
+        try
+        {
+            string CapacityString = System.IO.File.ReadAllText("/sys/class/power_supply/battery/capacity");
+            return int.Parse(CapacityString);
+        }
+        catch (Exception e)
+        {
+            Debug.Log("Failed to read battery power; " + e.Message);
+        }
+        return -1;
+    }
+    IEnumerator UpdataTime()
+    {
+        DateTime now = DateTime.Now;
+        Time.text += string.Format("{0}:{1}", now.Hour, now.Minute);
+        yield return new WaitForSeconds(60f - now.Second);
+        while (true)
+        {
+            now = DateTime.Now;
+            Time.text += "\n当前系统时间:" + string.Format("{0}:{1}", now.Hour, now.Minute);
+            yield return new WaitForSeconds(60f);
+        }
+    }
+}
---
+++ StartActionDemo/Assets/PicoMobileSDK/Pvr_UnitySDK/Scenes/Examples/StartAction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAction : MonoBehaviour {
+
+    string actionForSettings= "pui.settings.action.SETTINGS";
+    string actionForBluetooth = "android.settings.BLUETOOTH_SETTINGS";
+    string actionForWifi = "android.settings.WIFI_SETTINGS";
+
+    string actionForController = "android.settings.CONTROLLER_SETTINGS";
+    // Use this for initialization
+    void Start () {
+
+        
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	public void startActionSettings()
+	{
+		startToAction(actionForSettings);
+	}
+    public void startActionBluetooth()
+    {
+        startToAction(actionForBluetooth);
+    }
+    public void startActionWifi()
+    {
+        startToAction(actionForWifi);
+    }
+    public void startActionController()
+    {
+        startToAction(actionForController);
+    }
+    private void startToAction(string action)
+	{
+		AndroidJavaClass jcPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+		AndroidJavaObject joActivity = jcPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+		AndroidJavaObject joIntent = new AndroidJavaObject("android.content.Intent", action);
+		joActivity.Call("startActivity", joIntent);
+	}
+}
